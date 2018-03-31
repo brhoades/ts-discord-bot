0c1bdea2864a6b723f56b28d63a6d0bcd30ed017
Billy Rhoades <billyarhoades@gmail.com>
2018-03-31 09:23:59 -0500

    Added quit command to kill the bot.

---
+++ src/commands/botcontrol.ts
@@ -0,0 +1,25 @@
+import { Message } from 'discord.js';
+
+import Client from '../lib/client';
+import { Help, HelpCommand } from '../types/command';
+
+export const name = 'Bot Control';
+export const help: Help = {
+  commands: [
+    {
+      description: 'Kills the bot\'s connection to the server.',
+      invocation: `!quit`,
+      invocationTest: new RegExp(`^!quit`),
+      shortDescription: 'Kills the bot entirely.',
+    },
+  ],
+};
+
+export const register = (client: Client) => {
+  client.onCommand('quit', () => {
+    client.destroy();
+    client.on('disconnect', () => {
+      process.exit(0);
+    });
+  });
+};
